68079b1ba783727f795798f1e4adbb3261bae069
Half_nothing <Half_nothing@163.com>
2024-10-11 17:00:53 +0800

    feat: Cross-platform init

---
+++ fabric-like/src/main/java/net/superricky/tpaplusplus/fabriclike/ExampleModFabricLike.java
@@ -0,0 +1,10 @@
+package net.superricky.tpaplusplus.fabriclike;
+
+import net.superricky.tpaplusplus.ExampleMod;
+
+public final class ExampleModFabricLike {
+    public static void init() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
